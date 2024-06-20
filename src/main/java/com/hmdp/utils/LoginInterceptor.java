7d2afc2718eaf575bd73a62f97d9ed00fe45ef48
waiwaiwai128 <358172615@qq.com>
2024-06-20 10:44:57 +0800

    Initial Commit

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,38 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获 取session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户
+        Object user = session.getAttribute("user");
+        //3.判断用户是否存在
+        if(user == null){
+            //4.不存在 拦截
+            response.setStatus(401);
+            return false;
+        }
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        //5.存在 保存用户信息到threadLocal
+        UserHolder.saveUser(userDTO);
+        //6.放行
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
